9f738dd46b99cd0560e8107d0e43ad20c15c68a1
Rahul <rahul17a1997@gmail.com>
2020-05-12 11:41:59 +0530

    Delete product

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,7 +2,7 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
   
-  def  total_price
-    product.price * quantity
+  def total_price
+    product_price * quantity
   end
 end
