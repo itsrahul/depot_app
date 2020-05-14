67d0e042092af6391b76ad281c4b50a2e0a1f2e3
Rahul <rahul17a1997@gmail.com>
2020-05-14 09:32:12 +0530

    Task J,K(logging,i18n)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,12 +3,10 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @products = Product.order(:title)
-
-    if session[:counter].nil?
-      session[:counter] = 0
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
     else
-      session[:counter] = session[:counter] + 1
-    end 
+      @products = Product.order(:title)
+    end
   end
 end
