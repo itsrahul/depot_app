00865ba50e81c8abdcefee6fe1e445e705b13039
Rahul <rahul17a1997@gmail.com>
2020-06-19 19:30:26 +0530

    routing

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,11 @@ class Admin::CategoriesController < AdminBaseController
   def index
     @categories = Category.includes(:sub_categories)
   end
+
+  def show
+    # @category = Category.find(params[:id])
+    # @products = @category.products
+    # @products += @category.sub_category_products.map(&:products)[0] if @category.is_root?
+    redirect_to category_products_path(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,46 @@
 Rails.application.routes.draw do
-  get 'admin' => 'admin#index'
-  controller :sessions do
-    get 'login' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
-  end
-  get 'sessions/create'
-  get 'sessions/destroy'
+  root 'store#index',  as: 'store_index'
+  
+  # constraints(-> (req) { !req.env['HTTP_USER_AGENT'].include?("Firefox") }) do
+  constraints(-> (req) { req.env['HTTP_USER_AGENT'].include?("Firefox") }) do
+    get 'admin' => 'admin#index'
+    controller :sessions do
+      get 'login' => :new
+      post 'login' => :create
+      delete 'logout' => :destroy
+    end
+    get 'sessions/create'
+    get 'sessions/destroy'
 
-  resources :users do
-    collection do
-      get :orders
-      get :line_items
+    resources :users do
+      collection do
+        get :orders
+        get :line_items
+      end
+    end
+    resources :products, path: :books do
+      get :who_bought, on: :member, format: 'atom'
+    end
+    
+    resources :support_requests, only: [ :index, :update]
+
+    scope '(:locale)' do
+      resources :orders
+      resources :line_items
+      resources :carts
+      get 'my-orders', to: 'users#orders'
+      get 'my-items', to: 'users#line_items'
+      get 'categories/:id', to: 'store#index', constraints: { id: /\D.+/ }
+      # root 'store#index',  as: 'store_index'
+    end
+
+    namespace :admin do
+      resources :reports, only: :index
+      resources :categories, only: [:index, :show]
+    end
+
+    resources :categories do
+      resources :products, path: :books
     end
   end
-  resources :products do
-    get :who_bought, on: :member
-  end
-  
-  resources :support_requests, only: [ :index, :update]
-
-  scope '(:locale)' do
-    resources :orders
-    resources :line_items
-    resources :carts
-    root 'store#index',  as: 'store_index'
-  end
-
-  namespace :admin do
-    resources :reports, only: :index
-    resources :categories, only: :index
-  end
-  
 end
